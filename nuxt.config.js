75b42eaae815cec1125991dae5aa161efcc9bfe7
Nate <37554478+servusDei2018@users.noreply.github.com>
2021-04-13 19:45:28 -0400

    Add files via upload

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,8 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     { src: "~/plugins/vue-stack-grid.js", mode: "client" },
-    { src: "~/plugins/vue-cool-lightbox.js", mode: "client" }
+    { src: "~/plugins/vue-cool-lightbox.js", mode: "client" },
+    { src: "~/plugins/vue-loading.js", mode: "client" }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
